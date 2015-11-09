fedb6175cad7d57603f2bae1a4b7a58f3728463c
Autushka <aleh@conspector.com>
2015-11-09 10:36:53 -0500

    Update karma.conf.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
     preprocessors: {
       'app/js/**/*.js': ['browserify', 'coverage']
     },
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
     reporters: ['progress', 'coverage'],
 
     autoWatch: true,
